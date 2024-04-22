9594d25802b7de6e75026d90977aba2109837f8d
danilo-batista1 <danilo2221@gmail.com>
2024-04-22 10:07:34 -0300

    add contagem de veículos por fabricante

--- Sistema Gestão de Veículos/front-end/src/components/Marcas/index.tsx
+++ Sistema Gestão de Veículos/front-end/src/components/Marcas/index.tsx
@@ -3,15 +3,45 @@ import axios from 'axios';
 import { URI } from '../../enumerations/uri';
 import { Veiculos } from '../../types/veiculo';
 
-
-  
 function Marcas() {
+    const [veiculos, setVeiculos] = useState([]);
 
+    useEffect(() => {
+        fetchVeiculos();
+    }, []);
+
+    const fetchVeiculos = async () => {
+        try {
+            const response = await axios.get(`${URI.LISTAR_VEICULO}`);
+            setVeiculos(response.data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const contarVeiculosPorFabricante = (veiculos: Veiculos[]) => {
+        const contagem: { [marca: string]: number } = {};
+
+        veiculos.forEach(veiculo => {
+            const marca = veiculo.marca;
+
+            if (contagem[marca]) {
+                contagem[marca]++;
+            } else {
+                contagem[marca] = 1;
+            }
+        });
+
+        return contagem;
+    }
+    const contagemPorFabricante = contarVeiculosPorFabricante(veiculos);
 
     return (
         <div>
-            <ul style={{ listStyleType: 'none' }}>
-                
+            <ul style={{ listStyleType: 'none', fontFamily: 'Arial, Helvetica, sans-serif' }}>
+                {Object.entries(contagemPorFabricante).map(([marca, quantidade]) => (
+                    <li key={marca}>{marca} - {quantidade} veiculos</li>
+                ))}
             </ul>
         </div>
     );
